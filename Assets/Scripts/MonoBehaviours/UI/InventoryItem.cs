a4d5e0509d8a9a1f37dff35c27efda87fd057796
Nathan Blore <nathanblore97@gmail.com>
2024-03-06 02:38:32 +0000

    Item stacking, left and right click bug fixed

--- Assets/Scripts/MonoBehaviours/UI/InventoryItem.cs
+++ Assets/Scripts/MonoBehaviours/UI/InventoryItem.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-
     [Header("UI")]
     public Image image;
     public Text countText;
@@ -14,32 +14,97 @@ public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     [HideInInspector] public int count = 1;
     [HideInInspector] public Transform parentAfterDrag;
 
-    public void InitialiseItem(Item newItem) {
+    bool isDragging = false; // Track if dragging is occurring
+    private InventoryManager inventoryManager;
+
+
+
+    private void Start()
+    {
+        inventoryManager = FindObjectOfType<InventoryManager>(); // Find the InventoryManager in the scene
+    }
+    public void InitialiseItem(Item newItem)
+    {
         item = newItem;
         image.sprite = newItem.image;
         RefreshCount();
     }
 
-    public void RefreshCount() {
+    public void RefreshCount()
+    {
         countText.text = count.ToString();
         bool textActive = count > 1;
         countText.gameObject.SetActive(textActive);
     }
 
-    public void OnBeginDrag(PointerEventData eventData) {
-        image.raycastTarget = false;
-        parentAfterDrag = transform.parent;
-        transform.SetParent(transform.root);
-        countText.raycastTarget = false;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (!isDragging && (Mouse.current.leftButton.isPressed || (Mouse.current.leftButton.isPressed && Mouse.current.rightButton.isPressed)))
+        {
+            isDragging = true; // Start dragging if left button is pressed or both buttons are pressed
+            image.raycastTarget = false;
+            parentAfterDrag = transform.parent;
+            transform.SetParent(transform.root);
+            countText.raycastTarget = false;
+        }
     }
 
-    public void OnDrag(PointerEventData eventData) {
-        transform.position = Input.mousePosition;
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (isDragging)
+        {
+            transform.position = Mouse.current.position.ReadValue();
+        }
     }
 
-    public void OnEndDrag(PointerEventData eventData) {
-        image.raycastTarget = true;
-        transform.SetParent(parentAfterDrag);
-        countText.raycastTarget = true;
+    public void OnEndDrag(PointerEventData Data)
+    {
+        if (isDragging)
+        {
+            isDragging = false;
+            image.raycastTarget = true;
+            InventoryItem targetItem = Data.pointerEnter ? Data.pointerEnter.GetComponent<InventoryItem>() : null;
+
+            if (targetItem != null && item == targetItem.item && item.stackable)
+            {
+                // Snap the dragged item onto the target item slot
+                transform.SetParent(targetItem.transform.parent);
+                transform.position = targetItem.transform.position;
+                transform.SetAsLastSibling(); // Ensure the dragged item is rendered on top
+
+                // Merge the dragged item with the target item
+                targetItem.MergeWith(this);
+            }
+            else
+            {
+                transform.SetParent(parentAfterDrag);
+            }
+
+            countText.raycastTarget = true;
+        }
     }
+
+    public void MergeWith(InventoryItem otherItem)
+    {
+        int totalItemsCount = otherItem.count + count;
+        if (totalItemsCount <= inventoryManager.maxStackedItems)
+        {
+            // If the total count of items in the stack doesn't exceed the maximum stack size,
+            // merge the stacks completely
+            otherItem.count = totalItemsCount;
+            otherItem.RefreshCount();
+            Destroy(gameObject); // Destroy the dragged item
+        }
+        else
+        {
+            // If the target slot can't accommodate the entire stack, partially merge them
+            int spaceLeftInStack = inventoryManager.maxStackedItems - otherItem.count;
+            otherItem.count += spaceLeftInStack;
+            otherItem.RefreshCount();
+            count -= spaceLeftInStack;
+            RefreshCount();
+        }
+    }
+
+
 }
